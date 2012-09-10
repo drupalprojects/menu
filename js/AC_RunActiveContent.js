5285edd0cc84302d6ede5c5b778c72f13d8d5529
Web-Dorado <info@web-dorado.com>
2012-09-10 15:47:45 +0500

    58

--- js/AC_RunActiveContent.js
+++ js/AC_RunActiveContent.js
@@ -1,6 +1,8 @@
 /**
- * v1.7 Flash Player Version Detection Detect Client Browser type Copyright
- * 2005-2007 Adobe Systems Incorporated. All rights reserved.
+ * v1.7
+ * Flash Player Version Detection
+ * Detect Client Browser type
+ * Copyright 2005-2007 Adobe Systems Incorporated.  All rights reserved.
  */
 var isIE = (navigator.appVersion.indexOf("MSIE") != -1) ? true : false;
 var isWin = (navigator.appVersion.toLowerCase().indexOf("win") != -1) ? true
@@ -106,15 +108,15 @@ function GetSwfVer() {
     }
   }
   // MSN/WebTV 2.6 supports Flash 4
-  elseif (navigator.userAgent.toLowerCase().indexOf("webtv/2.6") != -1)
+  else if (navigator.userAgent.toLowerCase().indexOf("webtv/2.6") != -1)
     flashVer = 4;
   // WebTV 2.5 supports Flash 3
-  elseif (navigator.userAgent.toLowerCase().indexOf("webtv/2.5") != -1)
+  else if (navigator.userAgent.toLowerCase().indexOf("webtv/2.5") != -1)
     flashVer = 3;
   // older WebTV supports Flash 2
-  elseif (navigator.userAgent.toLowerCase().indexOf("webtv") != -1)
+  else if (navigator.userAgent.toLowerCase().indexOf("webtv") != -1)
     flashVer = 2;
-  elseif (isIE && isWin && !isOpera) {
+  else if (isIE && isWin && !isOpera) {
     flashVer = ControlVersion();
   }
   return flashVer;
@@ -131,13 +133,10 @@ function DetectFlashVer(reqMajorVer, reqMinorVer, reqRevision) {
   } else if (versionStr != 0) {
     if (isIE && isWin && !isOpera) {
       // Given "WIN 2,0,0,11"
-      // ["WIN", "2,0,0,11"]
-      tempArray = versionStr.split(" ");
-      // "2,0,0,11"
-      tempString = tempArray[1];
-      // ['2', '0', '0', '11']
-      versionArray = tempString.split(",");
-      } else {
+      tempArray = versionStr.split(" "); // ["WIN", "2,0,0,11"]
+      tempString = tempArray[1]; // "2,0,0,11"
+      versionArray = tempString.split(","); // ['2', '0', '0', '11']
+    } else {
       versionArray = versionStr.split(".");
     }
     var versionMajor = versionArray[0];
@@ -146,13 +145,12 @@ function DetectFlashVer(reqMajorVer, reqMinorVer, reqRevision) {
 
     if (versionMajor > parseFloat(reqMajorVer)) {
       return true;
-    } elseif (versionMajor == parseFloat(reqMajorVer)) {
+    } else if (versionMajor == parseFloat(reqMajorVer)) {
       if (versionMinor > parseFloat(reqMinorVer))
         return true;
-      elseif (versionMinor == parseFloat(reqMinorVer)) {
-        if (versionRevision >= parseFloat(reqRevision)) {
+      else if (versionMinor == parseFloat(reqMinorVer)) {
+        if (versionRevision >= parseFloat(reqRevision))
           return true;
-        }
       }
     }
     return false;
@@ -160,29 +158,27 @@ function DetectFlashVer(reqMajorVer, reqMinorVer, reqRevision) {
 }
 
 function AC_AddExtension(src, ext) {
-  if (src.indexOf('?') != -1) {
+  if (src.indexOf('?') != -1)
     return src.replace(/\?/, ext + '?');
-  }
-  else {
+  else
     return src + ext;
-  }
 }
 
 function AC_Generateobj(objAttrs, params, embedAttrs) {
   var str = '';
   if (isIE && isWin && !isOpera) {
     str += '<object ';
-    for (var i in objAttrs) {
+    for ( var i in objAttrs) {
       str += i + '="' + objAttrs[i] + '" ';
     }
     str += '>';
-    for (var i in params) {
+    for ( var i in params) {
       str += '<param name="' + i + '" value="' + params[i] + '" /> ';
     }
     str += '</object>';
   } else {
     str += '<embed ';
-    for (var i in embedAttrs) {
+    for ( var i in embedAttrs) {
       str += i + '="' + embedAttrs[i] + '" ';
     }
     str += '> </embed>';
@@ -209,24 +205,21 @@ function AC_GetArgs(args, ext, srcParamName, classid, mimeType) {
   ret.embedAttrs = new Object();
   ret.params = new Object();
   ret.objAttrs = new Object();
-  for (var i = 0; i < args.length; i = i + 2) {
+  for ( var i = 0; i < args.length; i = i + 2) {
     var currArg = args[i].toLowerCase();
 
     switch (currArg) {
     case "classid":
       break;
-
     case "pluginspage":
       ret.embedAttrs[args[i]] = args[i + 1];
       break;
-
     case "src":
     case "movie":
       args[i + 1] = AC_AddExtension(args[i + 1], ext);
       ret.embedAttrs["src"] = args[i + 1];
       ret.params[srcParamName] = args[i + 1];
       break;
-
     case "onafterupdate":
     case "onbeforeupdate":
     case "onblur":
@@ -269,7 +262,6 @@ function AC_GetArgs(args, ext, srcParamName, classid, mimeType) {
     case "id":
       ret.objAttrs[args[i]] = args[i + 1];
       break;
-
     case "width":
     case "height":
     case "align":
@@ -282,14 +274,12 @@ function AC_GetArgs(args, ext, srcParamName, classid, mimeType) {
     case "tabindex":
       ret.embedAttrs[args[i]] = ret.objAttrs[args[i]] = args[i + 1];
       break;
-
     default:
       ret.embedAttrs[args[i]] = ret.params[args[i]] = args[i + 1];
     }
   }
   ret.objAttrs["classid"] = classid;
-  if (mimeType) {
+  if (mimeType)
     ret.embedAttrs["type"] = mimeType;
-  }
   return ret;
 }
