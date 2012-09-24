783ab85a20231deb1c186786f99197fe300cec4b
Web-Dorado <info@web-dorado.com>
2012-09-24 15:23:22 +0500

    Issue #50 : Fixed bugs.

--- menu.php
+++ menu.php
@@ -1,32 +1,44 @@
 <?php
 /**
  * @file
- * menu.php
+ * This file generate xml that contains menus structure and appearance
+ * parameters which were set in Drupal module settings page.
  */
 
-chdir('../../../../');
+$str = getcwd();
+$str = implode("\\", explode("/", $str));
+$count = 0;
+$back_string = '';
+$index = strpos($str, '\\sites\\');
+while (strpos($str, '\\', $index)) {
+  $count++;
+  $back_string .= '../';
+  $index = strpos($str, '\\', $index) + 1;
+}
+
+chdir ($back_string);
 require_once 'includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 echo ('<dropdown>
     <settings
-    menuColor = "0x' . variable_get('menu_background_color', 'F2F2F2') . '"
-    menuFontColor = "0x' . variable_get('menu_text_color', '525A66') . '"
-    menuFontGlowColor = "0x' . variable_get('menu_hover_color', '7FBFFF') . '"
-    subMenuFontColor = "0x' . variable_get('submenu_text_color', '525A66') . '"
-    subMenuFontGlowColor = "0x' . variable_get('submenu_hover_color', '7FBFFF') . '"
+    menuColor = "0x' . variable_get('folder_menu_background_color', 'F2F2F2') . '"
+    menuFontColor = "0x' . variable_get('folder_menu_text_color', '525A66') . '"
+    menuFontGlowColor = "0x' . variable_get('folder_menu_hover_color', '7FBFFF') . '"
+    subMenuFontColor = "0x' . variable_get('folder_menu_submenu_text_color', '525A66') . '"
+    subMenuFontGlowColor = "0x' . variable_get('folder_menu_submenu_hover_color', '7FBFFF') . '"
     >
     </settings>');
-$menu_type = variable_get('folder_menu_menu_parent', 'primary-links');
+$menu_type = variable_get('folder_menu_parent', 'primary-links');
 $menus = menu_tree_all_data($menu_type);
 
 foreach ($menus as $menu) {
   $menulink = $menu['link'];
   if (strstr($menulink['link_path'], 'http')) {
-    echo '<menu cap="' . $menulink['title'] . '" url = "' . $menulink['link_path'] . '" window = "_self">';
+    echo '<menu cap="' . check_plain($menulink['title']) . '" url = "' . check_url($menulink['link_path']) . '" window = "_self">';
   }
   else {
-    echo '<menu cap="' . $menulink['title'] . '" url = "?q=' . $menulink['link_path'] . '" window = "_self">';
+    echo '<menu cap="' . check_plain($menulink['title']) . '" url = "?q=' . check_url($menulink['link_path']) . '" window = "_self">';
   }
 
   $submenus = $menu['below'];
@@ -34,10 +46,10 @@ foreach ($menus as $menu) {
     foreach ($submenus as $submenu) {
       $submenulink = $submenu['link'];
       if (strstr($submenulink['link_path'], 'http')) {
-        echo '<submenu cap="' . $submenulink['title'] . '" url = "' . $submenulink['link_path'] . '" window = "_self"></submenu>';
+        echo '<submenu cap="' . check_plain($submenulink['title']) . '" url = "' . check_url($submenulink['link_path']) . '" window = "_self"></submenu>';
       }
       else {
-        echo '<submenu cap="' . $submenulink['title'] . '" url = "?q=' . $submenulink['link_path'] . '" window = "_self"></submenu>';
+        echo '<submenu cap="' . check_plain($submenulink['title']) . '" url = "?q=' . check_url($submenulink['link_path']) . '" window = "_self"></submenu>';
       }
     }
   }
