3ea076a7d450d8b01a934f527b8048517f32cda3
Web-Dorado <info@web-dorado.com>
2012-09-24 15:34:33 +0500

    Issue #53 : Fixed bugs.

--- README.txt
+++ README.txt
@@ -1,50 +1,41 @@
+OVERVIEW
 
------------------------
-  OVERVIEW
-<<<<<<< HEAD
-----------------------Folder Menu is a flash dynamic menu module for your
-Drupal 6.x,7.x website, designed to meet your needs and preferences.
-=======
-----------------------Folder Menu is a flash dynamic menu module for your
-Drupal 6.x,7.x website, designed to meet your needs and preferences.
->>>>>>> 7f782039af819a9bc62aef68a25c844835b43366
+Folder Menu is a flash dynamic menu module for your
+Drupal 6.x website, designed to meet your needs and preferences.
 Main features:
 Possibility to customize the product using various parameters.
 All colors of all 3 flash menus are customizable.
 Possibility to fix the position of the menu during the scrolling.
 Nice Flash effects
-<<<<<<< HEAD
 Interface fully integrated with Drupal administration, which allows to use
 Folder Menu with new or existing menus of Drupal.
 Folder Menu makes the Menu very convenient and easy-to-use.
 Now you can organize your website pages more easily and present user-friendly
 websites to your customers and visitors.
-=======
-Interface fully integrated with Drupal administration, which allows to use
-Folder Menu with new or existing menus of Drupal.
-Folder Menu makes the Menu very convenient and easy-to-use.
-Now you can organize your website pages more easily and present user-friendly
-websites to your customers and visitors.
->>>>>>> 7f782039af819a9bc62aef68a25c844835b43366
------------------------
-  INSTALLATION
------------------------
 
 
-To install Folder Menu module
-1. Download and enable the module
-2. Go to Administrator > Site building > Folder Menu settings
-Menu Parent: Choose the menu parent.
-Menu Position: Choose the position of Folder Menu.
-Distance from top: Define the distance of Folder Menu from the top.
-Menu Font: Choose the font of Folder Menu.
-Menu Background Color: Choose the background color of Folder menu.
-Menu Text Color: Choose the text color of Folder menu.
-Menu Hover Color: Choose the hover color of Folder menu.
-Submenu Hover Color: Choose the submenu hover color of Folder menu.
------------------------
-  AUTHOR INFORMATION
------------------------
+INSTALLATION
+
+To install Folder Menu module:
+1. Download and enable the module.
+2. Go to Administer > Site configuration > Folder Menu settings.
+  Menu Parent: Choose the menu parent.
+  Menu Position: Choose the position of Folder Menu.
+  Distance from top: Define the distance of Folder Menu from the top.
+  Menu Font: Choose the font of Folder Menu.
+  Menu Background Color: Choose the background color of Folder menu.
+  Menu Text Color: Choose the text color of Folder menu.
+  Menu Hover Color: Choose the hover color of Folder menu.
+  Submenu Hover Color: Choose the submenu hover color of Folder menu.
+3. On Administer > Site configuration > Blocks menu
+select Folder Menu block Region.
+4. Change Folder Menu module permissions on
+Administer > User management > Permissions menu.
+5. You also can find help topics and get Folder Menu module full version on
+Administer > Help > Folder Menu menu.
+
+
+AUTHOR INFORMATION
 
 Written by Webdorado team 
 homepage: www.web-dorado.com
--- menu.php
+++ menu.php
@@ -1,50 +1,55 @@
 <?php
 /**
  * @file
- * menu.php
+ * This file generate xml that contains menus structure and appearance
+ * parameters which were set in Drupal module settings page.
  */
 
-define('DRUPAL_ROOT', '../../../../');
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+$str = getcwd();
+$str = implode("\\", explode("/", $str));
+$count = 0;
+$back_string = '';
+$index = strpos($str, '\\sites\\');
+while (strpos($str, '\\', $index)) {
+  $count++;
+  $back_string .= '../';
+  $index = strpos($str, '\\', $index) + 1;
+}
+
+chdir ($back_string);
+require_once 'includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 echo ('<dropdown>
     <settings
-    menuColor = "0x' . variable_get('menu_background_color', 'F2F2F2') . ' "
-    menuFontColor = "0x' . variable_get('menu_text_color', '525A66') . ' "
-    menuFontGlowColor = "0x' . variable_get('menu_hover_color', '7FBFFF') . '"
-    subMenuFontColor = "0x' . variable_get('submenu_text_color', '525A66') . '"
-    subMenuFontGlowColor = "0x' . variable_get('submenu_hover_color', '7FBFFF') . '"
+    menuColor = "0x' . variable_get('folder_menu_background_color', 'F2F2F2') . '"
+    menuFontColor = "0x' . variable_get('folder_menu_text_color', '525A66') . '"
+    menuFontGlowColor = "0x' . variable_get('folder_menu_hover_color', '7FBFFF') . '"
+    subMenuFontColor = "0x' . variable_get('folder_menu_submenu_text_color', '525A66') . '"
+    subMenuFontGlowColor = "0x' . variable_get('folder_menu_submenu_hover_color', '7FBFFF') . '"
     >
     </settings>');
-
-$menu_type = variable_get('folder_menu_menu_parent', 'primary-links');
+$menu_type = variable_get('folder_menu_parent', 'primary-links');
 $menus = menu_tree_all_data($menu_type);
 
 foreach ($menus as $menu) {
-  $menu_link = $menu['link'];
-
-  if (strstr($menu_link['link_path'], 'http')) {
-    echo '<menu cap="' . $menu_link['title'] . '" url = "' . $menu_link['link_path'] . '" window = "_self">';
+  $menulink = $menu['link'];
+  if (strstr($menulink['link_path'], 'http')) {
+    echo '<menu cap="' . check_plain($menulink['title']) . '" url = "' . check_url($menulink['link_path']) . '" window = "_self">';
   }
   else {
-    if ($menu_link['link_path'] == '<front>') {
-      echo '<menu cap="' . $menu_link['title'] . '" url = "?q=" window = "_self">';
-    }
-    else {
-      echo '<menu cap="' . $menu_link['title'] . '" url = "?q=' . $menu_link['link_path'] . '" window = "_self">';
-    }
+    echo '<menu cap="' . check_plain($menulink['title']) . '" url = "?q=' . check_url($menulink['link_path']) . '" window = "_self">';
   }
 
   $submenus = $menu['below'];
   if ($submenus != FALSE) {
     foreach ($submenus as $submenu) {
-      $submenu_link = $submenu['link'];
-      if (strstr($submenu_link['link_path'], 'http')) {
-        echo '<submenu cap="' . $submenu_link['title'] . '" url = "' . $submenu_link['link_path'] . '" window = "_self"></submenu>';
+      $submenulink = $submenu['link'];
+      if (strstr($submenulink['link_path'], 'http')) {
+        echo '<submenu cap="' . check_plain($submenulink['title']) . '" url = "' . check_url($submenulink['link_path']) . '" window = "_self"></submenu>';
       }
       else {
-        echo '<submenu cap="' . $submenu_link['title'] . '" url = "?q=' . $submenu_link['link_path'] . '" window = "_self"></submenu>';
+        echo '<submenu cap="' . check_plain($submenulink['title']) . '" url = "?q=' . check_url($submenulink['link_path']) . '" window = "_self"></submenu>';
       }
     }
   }
