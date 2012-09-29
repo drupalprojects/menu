4d76f493aad6157ee7d65bf00bc9a589374ed696
Web-Dorado <info@web-dorado.com>
2012-09-29 14:32:18 +0500

    Issue #72 : Checked disable menus.

--- menu.php
+++ menu.php
@@ -30,37 +30,37 @@ echo ('<dropdown>
 
 $menu_type = variable_get('folder_menu_parent', 'primary-links');
 $menus = menu_tree_all_data($menu_type);
-if (count($menus) > 3) {
-  $menus = array_slice($menus, 0, 3);
-}
 
 foreach ($menus as $menu) {
   $menu_link = $menu['link'];
-
-  if (strstr($menu_link['link_path'], 'http')) {
-    echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "' . check_url($menu_link['link_path']) . '" window = "_self">';
-  }
-  else {
-    if ($menu_link['link_path'] == '<front>') {
-      echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "?q=" window = "_self">';
+  if ($menu_link['hidden'] == 0) {
+    if (strstr($menu_link['link_path'], 'http')) {
+      echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "' . check_url($menu_link['link_path']) . '" window = "_self">';
     }
     else {
-      echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "?q=' . check_url($menu_link['link_path']) . '" window = "_self">';
+      if ($menu_link['link_path'] == '<front>') {
+        echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "?q=" window = "_self">';
+      }
+      else {
+        echo '<menu cap="' . check_plain($menu_link['title']) . '" url = "?q=' . check_url($menu_link['link_path']) . '" window = "_self">';
+      }
     }
-  }
 
   $submenus = $menu['below'];
   if ($submenus != FALSE) {
     foreach ($submenus as $submenu) {
       $submenu_link = $submenu['link'];
-      if (strstr($submenu_link['link_path'], 'http')) {
-        echo '<submenu cap="' . check_plain($submenu_link['title']) . '" url = "' . check_url($submenu_link['link_path']) . '" window = "_self"></submenu>';
-      }
-      else {
-        echo '<submenu cap="' . check_plain($submenu_link['title']) . '" url = "?q=' . check_url($submenu_link['link_path']) . '" window = "_self"></submenu>';
+      if ($submenu_link['hidden'] == 0) {
+        if (strstr($submenu_link['link_path'], 'http')) {
+          echo '<submenu cap="' . check_plain($submenu_link['title']) . '" url = "' . check_url($submenu_link['link_path']) . '" window = "_self"></submenu>';
+        }
+        else {
+          echo '<submenu cap="' . check_plain($submenu_link['title']) . '" url = "?q=' . check_url($submenu_link['link_path']) . '" window = "_self"></submenu>';
+        }
       }
     }
   }
   echo '</menu>';
+  }
 }
 echo '</dropdown>';
