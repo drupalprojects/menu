4bba349d430942ac4778c2363f711768f314e3da
Web-Dorado <info@web-dorado.com>
2012-10-02 13:02:02 +0500

    Issue #71 : Checked the Drupal coding standards.

--- js/folder_menu_createFlashdiv.js
+++ js/folder_menu_createFlashdiv.js
@@ -2,7 +2,7 @@
  * Return flash div.
  */
 function folder_menu_createFlash() {
-  if (typeof(AC_FL_RunContent)!="undefined") {
+  if (typeof(AC_FL_RunContent) != "undefined") {
     AC_FL_RunContent(
         "codebase", "http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0",
         "width", "1000",
@@ -23,10 +23,10 @@ function folder_menu_createFlash() {
         "allowScriptAccess","always",
         "movie", Drupal.settings.folder_menu.swf_url,
         "salign", "t",
-        "flashvars", "xmlUrl="+Drupal.settings.folder_menu.menu_xml_url
+        "flashvars", "xmlUrl=" + Drupal.settings.folder_menu.menu_xml_url
     );
 
-    if(typeof(FolderMenuVerticalElem)=="undefined") {
+    if(typeof(FolderMenuVerticalElem) == "undefined") {
       folder_menu_initFlash_Moz_vertical();
     }
     else {
@@ -39,8 +39,8 @@ function folder_menu_createFlash() {
   }
 }
 function initFlash_vertical()	{
-  if(typeof(FolderMenuVerticalElem.getWidth)=="function") {
-    FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",FolderMenuVerticalElem,"FolderMenuVerticalObj", initFlash_vertical);
+  if(typeof(FolderMenuVerticalElem.getWidth) == "function") {
+    FolderMenuVerticalObj = new FolderMenuVertical("foldermenuverticalContainer", FolderMenuVerticalElem, "FolderMenuVerticalObj", initFlash_vertical);
   }
   else {
     setTimeout("initFlash_vertical()",500);
@@ -48,23 +48,23 @@ function initFlash_vertical()	{
 }
 
 function folder_menu_onmouseover_vertical() {
-  if(typeof(FolderMenuVerticalObj)!="undefined")	{
-    FolderMenuVerticalObj.TOtrigger=true;
+  if(typeof(FolderMenuVerticalObj) != "undefined")	{
+    FolderMenuVerticalObj.TOtrigger = true;
     FolderMenuVerticalObj.setHeightAndWidthTO();
   }
 }
 
 function folder_menu_onmouseout_vertical() {
-  if(typeof(FolderMenuVerticalObj)!="undefined")	{
-    FolderMenuVerticalObj.TOtrigger=false;
+  if(typeof(FolderMenuVerticalObj) != "undefined")	{
+    FolderMenuVerticalObj.TOtrigger = false;
   }
 }
 
 function folder_menu_initFlash_Moz_vertical() {
-  if(typeof(document.FolderMenuVerticalElem.getWidth)=="function") {
-    FolderMenuVerticalObj=new FolderMenuVertical("foldermenuverticalContainer",document.FolderMenuVerticalElem,"FolderMenuVerticalObj", folder_menu_initFlash_Moz_vertical);
+  if (typeof(document.FolderMenuVerticalElem.getWidth) == "function") {
+    FolderMenuVerticalObj = new FolderMenuVertical("foldermenuverticalContainer", document.FolderMenuVerticalElem, "FolderMenuVerticalObj", folder_menu_initFlash_Moz_vertical);
   }
   else {
-    setTimeout("folder_menu_initFlash_Moz_vertical()",500);
+    setTimeout("folder_menu_initFlash_Moz_vertical()", 500);
   }
 }
